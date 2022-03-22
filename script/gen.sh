0718af46ca8d2c0d49f0b9f75154d75a0b51bc68
isaachan <isaachanstar@gmail.com>
2022-03-22 16:13:32 +0800

    add one more proto

--- script/gen.sh
+++ script/gen.sh
@@ -2,3 +2,7 @@ rm -rf ../src/gen && mkdir -p ../src/gen
 
 protoc -I../proto search.proto --cpp_out=../src/gen
 protoc -I../proto search.proto --grpc_out=../src/gen --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` 
+
+protoc -I../proto route_guide.proto --cpp_out=../src/gen
+protoc -I../proto route_guide.proto --grpc_out=../src/gen --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` 
+
